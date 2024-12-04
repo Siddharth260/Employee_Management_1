2e3d59a40ac28014865269c19e813bbc84c279e7
Siddharth Mishra <siddharthmishra2605@gmail.com>
2024-12-04 11:33:14 +0530

    Initial_Commit

---
+++ EmployeeManagement_1/Services/LeadService.cs
@@ -0,0 +1,170 @@
+﻿using EmployeeManagement_1.Common;
+using EmployeeManagement_1.Cosmos;
+using EmployeeManagement_1.Entities;
+using EmployeeManagement_1.Interface;
+using EmployeeManagement_1.Models;
+
+namespace EmployeeManagement_1.Services
+{
+    public class LeadService : ILeadService
+    {
+        private readonly ICosmosDbService _dbService;
+        public LeadService (ICosmosDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<Lead> AddEmployee(Lead leadModel)
+        {
+            var employee = new LeadEntity();
+            employee.Name = leadModel.Name;
+            employee.Designation = leadModel.Designation;
+            employee.Domain = leadModel.Domain;
+            employee.DateOfJoining = DateTime.Today;
+
+            if (employee.Designation == "Lead")
+            {
+                employee.initialize(true, Credentials.leadDocumentType, Credentials.createdBy, Credentials.createdByName);
+            }
+            else if (employee.Designation == "Member")
+            {
+                employee.initialize(true, Credentials.memberDocumentType, Credentials.createdBy, Credentials.createdByName);
+            }
+
+            var response = await _dbService.AddItemAsync(employee);
+
+            var responseModel = new Lead()
+            {
+                UId = employee.UId,
+                Name = employee.Name,
+                Designation = employee.Designation,
+                Domain = employee.Domain,
+                DateOfJoining = employee.DateOfJoining
+            };
+
+            return responseModel;
+            
+        }
+
+        public async Task<List<Lead>> GetAllEmployee()
+        {
+            var response = await _dbService.GetAllEmployee();
+            var responseList = new List<Lead>();
+            
+            foreach (var item in response)
+            {
+                var model = new Lead()
+                {
+                    UId = item.UId,
+                    Name = item.Name,
+                    DateOfJoining = item.DateOfJoining,
+                    Designation = item.Designation,
+                    Domain = item.Domain
+                };
+                responseList.Add(model);
+            }
+            return responseList;
+        }
+
+        public async Task<Lead> GetEmployeeByUId(string UId)
+        {
+            var response = await _dbService.GetEmployeeByUId(UId);
+            var model = new Lead()
+            {
+                UId = response.UId,
+                Name = response.Name,
+                DateOfJoining = response.DateOfJoining,
+                Designation = response.Designation,
+                Domain = response.Domain
+            };
+            return model;
+        }
+        public async Task<List<Lead>> GetMyTeamByUId(string UId)
+        {
+            var lead = await _dbService.GetEmployeeByUId(UId);
+            var response = await _dbService.GetMyTeamByUId(lead.Domain);
+            var responseList = new List<Lead>();
+
+            foreach (var item in response)
+            {
+                var model = new Lead()
+                {
+                    UId = item.UId,
+                    Name = item.Name,
+                    DateOfJoining = item.DateOfJoining,
+                    Designation = item.Designation,
+                    Domain = item.Domain
+                };
+                responseList.Add(model);
+            }
+            return responseList;
+        }
+
+        public async Task<Lead> UpdateEmployeeByUId(Lead leadModel)
+        {
+            var existingEmployee = await _dbService.GetEmployeeByUId(leadModel.UId);
+            existingEmployee.Active = false;
+            existingEmployee.Archived = true;
+            //existingEmployee.dType = leadModel.Designation;
+
+            await _dbService.ReplaceAsync(existingEmployee);
+
+            if (existingEmployee.Designation == "Lead")
+            {
+                existingEmployee.initialize(false, Credentials.leadDocumentType, Credentials.createdBy, Credentials.createdByName);
+            }
+            else if (existingEmployee.Designation == "Member")
+            {
+                existingEmployee.initialize(false, Credentials.memberDocumentType, Credentials.createdBy, Credentials.createdByName);
+            }
+
+            existingEmployee.Name = leadModel.Name;
+            existingEmployee.Designation = leadModel.Designation;
+            existingEmployee.Domain = leadModel.Domain;
+            existingEmployee.DateOfJoining = DateTime.Today;
+
+            var response = await _dbService.AddItemAsync(existingEmployee);
+
+            var responseModel = new Lead()
+            {
+                Name = existingEmployee.Name,
+                UId = existingEmployee.UId,
+                Designation = existingEmployee.Designation,
+                Domain = existingEmployee.Domain,
+                DateOfJoining = existingEmployee.DateOfJoining
+            };
+            return responseModel;
+        }
+
+        public async Task<string> RemoveEmployeeByUId(string UId)
+        {
+            var existingEmployee = await _dbService.GetEmployeeByUId(UId);
+            existingEmployee.Active = false;
+            existingEmployee.Archived = true;
+
+            await _dbService.ReplaceAsync(existingEmployee);
+
+            return "Employee Removed From The Company";
+        }
+
+        public async Task<List<Member>> GetTeamTaskSheetByUId(string UId)
+        {
+            var lead = await _dbService.GetEmployeeByUId(UId);
+            var teamList = await _dbService.GetMyTeamByUId(lead.Domain);
+            var taskList = new List<Member>();
+
+            foreach (var member in teamList)
+            {
+                var response = await _dbService.GetTaskSheetByUId(member.UId);
+                var model = new Member()
+                {
+                    UId = response.UId,
+                    Name = response.Name,
+                    Tasksheet = response.Tasksheet
+                };
+                taskList.Add(model);
+            }
+            return taskList;
+        }
+    }
+}
---
+++ EmployeeManagement_1/Services/MemberService.cs
@@ -0,0 +1,58 @@
+﻿using EmployeeManagement_1.Common;
+using EmployeeManagement_1.Cosmos;
+using EmployeeManagement_1.Entities;
+using EmployeeManagement_1.Interface;
+using EmployeeManagement_1.Models;
+using System.Drawing;
+
+namespace EmployeeManagement_1.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly ICosmosDbService _dbService;
+        public MemberService (ICosmosDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<Member> AddTaskSheetByUId(Member memberModel)
+        {
+            var task = new MemberEntity();
+            task.Name = memberModel.Name;
+            task.Tasksheet = memberModel.Tasksheet;
+            task.initialize(true, Credentials.taskDocumentType, Credentials.createdBy, Credentials.createdByName);
+            task.UId = memberModel.UId;
+
+            var response = await _dbService.AddItemAsync(task);
+
+            var responseModel = new Member
+            {
+                UId = response.UId,
+                Name = memberModel.Name,
+                Tasksheet = memberModel.Tasksheet
+            };
+
+            return responseModel;
+        }
+
+        public async Task<Member> GetTaskSheetByUId(string UId)
+        {
+            var task = await _dbService.GetTaskSheetByUId(UId);
+            var model = new Member
+            {
+                UId = task.UId,
+                Name = task.Name,
+                Tasksheet = task.Tasksheet
+            };
+            return model;
+        }
+        public async Task<string> ResignByUId(string UId)
+        {
+            var existingEmployee = await _dbService.GetEmployeeByUId(UId);
+            existingEmployee.Active = false;
+            existingEmployee.Archived = true;
+            await _dbService.ReplaceAsync(UId);
+            return "You have resigned";
+        }
+    }
+}
